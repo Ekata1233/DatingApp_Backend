d03719ebc8e50db6842c8ef758310f9c85ea4450
aniket-patil7075 <akp7075@gmail.com>
2026-02-09 10:27:48 +0530

    start working on gender api

--- src/features/onboarding/interestedIn/interestedIn.controller.ts
+++ src/features/onboarding/interestedIn/interestedIn.controller.ts
@@ -1,9 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-// import {
-//   getInterestedInCache,
-//   setInterestedInCache,
-//   clearInterestedInCache,
-// } from "./interestedIn.cache";
 import {
   createInterestedIn,
   getAllInterestedIn,
@@ -16,7 +11,6 @@ export const create = async (
 ) => {
   try {
     const data = await createInterestedIn(req.body);
-    // await clearInterestedInCache();
 
     res.status(201).json({ success: true, data });
   } catch (error) {
@@ -30,14 +24,7 @@ export const getAll = async (
   next: NextFunction
 ) => {
   try {
-    // const cached = await getInterestedInCache();
-
-    // if (cached) {
-    //   return res.json({ success: true, cached: true, data: cached });
-    // }
-
     const data = await getAllInterestedIn();
-    // await setInterestedInCache(data);
 
     res.json({ success: true, cached: false, data });
   } catch (error) {
