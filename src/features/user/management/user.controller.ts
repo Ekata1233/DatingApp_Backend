bf286efce96764940483b2cd83d41dc17efb3c65
aniket-patil7075 <akp7075@gmail.com>
2026-03-06 17:31:05 +0530

    get all users api done

--- src/features/user/management/user.controller.ts
+++ src/features/user/management/user.controller.ts
@@ -2,7 +2,19 @@ import { Request, Response } from "express";
 import * as userService from "./user.service";
 
 export const getAllUsersController = async (req: Request, res: Response) => {
-  const users = await userService.getAllUsers();
-  res.json(users);
-};
+  try {
+    const users = await userService.getAllUsers();
 
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully",
+      data: users,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
