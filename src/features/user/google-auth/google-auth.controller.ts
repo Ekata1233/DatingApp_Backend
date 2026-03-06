fc91ea56ceb50293224ce3bcf37c539bb50e91c1
aniket-patil7075 <akp7075@gmail.com>
2026-03-06 16:29:05 +0530

    google verification done

--- src/features/user/google-auth/google-auth.controller.ts
+++ src/features/user/google-auth/google-auth.controller.ts
@@ -3,14 +3,14 @@ import { googleLoginService } from "./google-auth.service";
 
 export const googleLoginController = async (req: Request, res: Response) => {
   try {
-    console.log("Google Login Request Body:", req.body); // Debugging log
+    console.log("Google Login Request Body:", req.body); 
     const { idToken } = req.body;
 
     const data = await googleLoginService(idToken);
 
     res.json(data);
   } catch (error) {
-    console.log("Google Login Error:", error); // ✅ add this
+    console.log("Google Login Error:", error); 
     res.status(400).json({ message: "Google Authentication Failed" });
   }
 };
