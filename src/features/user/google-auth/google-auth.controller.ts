20eab48c307b4fa40eb2b87b62041dce03c0a3c1
Ekata1233 <jadhavjanki8462@gmail.com>
2026-03-06 16:22:42 +0530

    solve prisma error

--- src/features/user/google-auth/google-auth.controller.ts
+++ src/features/user/google-auth/google-auth.controller.ts
@@ -3,12 +3,19 @@ import { googleLoginService } from "./google-auth.service";
 
 export const googleLoginController = async (req: Request, res: Response) => {
   try {
+    console.log("request",req);
+    
     const { idToken } = req.body;
 
     const data = await googleLoginService(idToken);
 
     res.json(data);
-  } catch (error) {
-    res.status(400).json({ message: "Google Authentication Failed" });
-  }
+  } catch (error: any) {
+  console.log("Google Auth Error 👉", error.response?.data || error.message);
+  res.status(400).json({
+    message: "Google Authentication Failed",
+    error: error.message,
+  });
+}
+
 };
