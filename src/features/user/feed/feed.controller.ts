a28d2426c8446161213dbee2c250eb895bfd0bc7
Ekata1233 <jadhavjanki8462@gmail.com>
2026-04-16 18:14:12 +0530

    feed api done

--- src/features/user/feed/feed.controller.ts
+++ src/features/user/feed/feed.controller.ts
@@ -2,21 +2,33 @@
 
 import { Request, Response } from "express";
 import { getFeedService } from "./feed.service";
+import { FeedMode } from "./feed.types";
 
-export const getFeedController = async (
-  req: Request,
-  res: Response
-) => {
+export const getFeedController = async (req: Request, res: Response) => {
   try {
+    // now user.id is available
     const userId = (req as any).user.id;
 
-    const { cursor, limit = 10 } = req.query;
+    const { cursor, limit = 10, mode } = req.query;
+
+    // ✅ Validate mode
+    if (
+      !mode ||
+      !["date_to_marry", "dating", "mature_connection"].includes(
+        mode as string
+      )
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or missing mode",
+      });
+    }
 
     const data = await getFeedService({
       userId,
       cursor: cursor as string,
       limit: Number(limit),
-      filter: req.query.filter as string,
+     mode: mode as FeedMode,
     });
 
     res.json({
--- src/features/user/messages/message.route.ts
+++ src/features/user/messages/message.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import { getMessages, sendMessage } from "./message.controller";
 
-router.get("/:receiverId", getMessages);
+// router.get("/:receiverId", getMessages);
 router.post("/message", sendMessage);
 
 export default router;
