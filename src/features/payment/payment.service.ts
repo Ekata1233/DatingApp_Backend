8d8ea34cdc32d2b1fa07efc1d792a17ec4606741
aniket-patil7075 <akp7075@gmail.com>
2026-07-24 13:03:40 +0530

    change in crate payment

--- src/features/payment/payment.service.ts
+++ src/features/payment/payment.service.ts
@@ -297,8 +297,8 @@ export async function createPaymentLink(userId: string, body: any) {
     source: "API",
     order_id: orderId,
 
-    successURL: "https://dating-app-backend-plum.vercel.app/api/payments/return",
-    failureURL: "https://www.fetchtrue.com/",
+    successURL: "https://www.welvors.com/stepdone/",
+    failureURL: "https://www.welvors.com/stepdone/",
 
 
     customer: {
