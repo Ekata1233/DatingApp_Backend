a26068a472f85c087ed2aeacff2e2824f1e90708
Ekata1233 <jadhavjanki8462@gmail.com>
2026-08-08 13:19:24 +0530

    ts error solve

--- src/features/user/user.repository.ts
+++ src/features/user/user.repository.ts
@@ -1,45 +1,45 @@
-import connectPostgres from "../../config/db";
+// import connectPostgres from "../../config/db";
 
 
-const pool = connectPostgres();
-interface CreateUserData {
-  name: string;
-  email: string;
-  password: string;
-}
+// const pool = connectPostgres();
+// interface CreateUserData {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
 
-interface User {
-  id: string;
-  name: string;
-  email: string;
-}
+// interface User {
+//   id: string;
+//   name: string;
+//   email: string;
+// }
 
-export const getUserById = async (id: string): Promise<User | undefined> => {
+// export const getUserById = async (id: string): Promise<User | undefined> => {
 
-  const result = await pool.query(
-    "SELECT id,name,email FROM users WHERE id=$1",
-    [id]
-  );
+//   const result = await pool.query(
+//     "SELECT id,name,email FROM users WHERE id=$1",
+//     [id]
+//   );
 
-  return result.rows[0];
-};
+//   return result.rows[0];
+// };
 
-export const getUserByEmail = async (email: string): Promise<any> => {
+// export const getUserByEmail = async (email: string): Promise<any> => {
 
-  const result = await pool.query(
-    "SELECT * FROM users WHERE email=$1",
-    [email]
-  );
+//   const result = await pool.query(
+//     "SELECT * FROM users WHERE email=$1",
+//     [email]
+//   );
 
-  return result.rows[0];
-};
+//   return result.rows[0];
+// };
 
-export const createUser = async (data: CreateUserData): Promise<User> => {
+// export const createUser = async (data: CreateUserData): Promise<User> => {
 
-  const result = await pool.query(
-    "INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING id,name,email",
-    [data.name, data.email, data.password]
-  );
+//   const result = await pool.query(
+//     "INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING id,name,email",
+//     [data.name, data.email, data.password]
+//   );
 
-  return result.rows[0];
-};
+//   return result.rows[0];
+// };
